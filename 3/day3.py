4db0d56487645adc1091cf1dc429a4dac1fac637
BuildTools <unconfigured@null.spigotmc.org>
2022-12-03 20:06:20 +0100

    Clean code and rewrite exo1 that was suppress

--- 3/day3.py
+++ 3/day3.py
@@ -1,33 +1,55 @@
 f = open("files/final", "r")
 
-rucksacks = []
-l = []
-count = 0
-
-clist = []
-
-def find_common(list):
-    for i in range(len(list)):
-        for let in list[0]:
-            if let in list[1] and let in list[2]:
-                return let
+lines = []
 
 for line in f.readlines():
-    n = len(line)
-    print(line)
-    l.append(line.strip())
-    count += 1
-    if count == 3:
-        clist.append(find_common(l))
-        l = []
+    lines.append(line.strip())
+
+def find_common(list):
+    size = len(list) - 1
+    count = 0
+    for let in list[0]:
+        n = size
+        while n > 0:
+            if let in list[n]:
+                count += 1
+            n -= 1
+        if count == size:
+            return let
         count = 0
+    raise RuntimeError("Could not find common")
 
-score = 0
+def get_score(slist):
+    score = 0
 
-for letter in clist:
-    if letter >= 'A' and letter <= 'Z':
-        score += (ord(letter) - 38)
-    else:
-        score += (ord(letter) - 96)
+    for letter in slist:
+        if letter >= 'A' and letter <= 'Z':
+            score += (ord(letter) - 38)
+        else:
+            score += (ord(letter) - 96)
+    return score
 
-print(score)
+def exo1():
+    clist = []
+    for line in lines:
+        clist.append(find_common([line[:len(line)//2], line[len(line)//2:]]))
+    return clist
+
+
+def exo2():
+    clist = []
+    l = []
+    count = 0
+    for line in lines:
+        l.append(line.strip())
+        count += 1
+        if count == 3:
+            clist.append(find_common(l))
+            l = []
+            count = 0
+    return clist
+
+slist = exo1()
+print(f'Exo 1 = {get_score(slist)}')
+slist = exo2()
+print(f'Exo 2 = {get_score(slist)}')
